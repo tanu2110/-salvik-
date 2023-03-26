75c9db831526f8eb5784ae24376a45066365f853
tanu2110 <tanushree.tnay@gmail.com>
2023-03-27 03:32:14 +0530

    new commit

--- src/containers/Social.js
+++ src/containers/Social.js
@@ -9,11 +9,23 @@ export function Comments() {
         <div className="title-social">
           <h1> Salvik Social</h1>
         </div>
+      </div>
+      <div className="group">
         <div className="sidebar">
-          <p>Trending Topics</p>
-          <ul>
-            <li>Trending Topics</li>
-          </ul>
+          <p>
+            Trending Topics
+            <img src="/images/icons/chevron-right.png" alt="" />
+          </p>
+          <ol>
+            <li>number 1</li>
+          </ol>
+        </div>
+        <div className="discussion">
+          <p>
+            <img src="/images/users/2.png" alt="user" /> Username1
+          </p>
+          <br />
+          <p>Comment1</p>
         </div>
       </div>
     </section>
